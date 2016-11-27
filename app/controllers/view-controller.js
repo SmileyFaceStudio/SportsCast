b1148f9121a42a9759144f41a7698f22e3c18054
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-11-27 01:25:01 -0800

    [feature] obtained Url's and upvote scores

--- app/controllers/view-controller.js
+++ app/controllers/view-controller.js
@@ -1,3 +1,5 @@
+import getUrl from 'get-urls';
+
 export default function ViewController () {
   return ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
 
@@ -5,7 +7,7 @@ export default function ViewController () {
 
     $http.get('https://www.reddit.com/r/nbastreams/new.json').success(function(response) {
     	let redditData = response.data.children;
-      var gameThread= [];
+      var gameThread = [];
       var thread = '';
       angular.forEach(redditData, function(value, key) {
       	if (value.data.link_flair_text === 'Game Thread') {
@@ -18,14 +20,33 @@ export default function ViewController () {
           thread = value.url;
       })
 
+      console.log(thread);
+
       if (thread)
       	$http.get(thread + '.json').success(function(response) {
       		console.log('thread:', response);
+          handleRedditAPI(response);
       	});
     });
 
     var handleRedditAPI = function(data) {
+      var threadData = [];
+      var threadLinks = [];
+      angular.forEach(data, function(value, key) {
+        threadData.push(value.data.children);
+      })
+      console.log(threadData);
+      threadData = threadData.reduce(function(a, b) { 
+        return a.concat(b);
+      }).filter(function (value) {
+        return value.kind == 't1';
+      });
+      angular.forEach(threadData, function(value, key) {
+        var upsAndLinks = {'ups': value.data.ups, 'urls': getUrl(value.data.body)};
+        threadLinks.push(upsAndLinks);
+      })
 
+      console.log(threadLinks);
     }
   }];
 }
